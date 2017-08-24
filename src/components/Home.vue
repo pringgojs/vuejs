25d976b1128d7b7ac8e79bdd660ca49c276b1ecd
Pringgo Js <odyinggo@gmail.com>
2017-08-24 21:12:06 +0700

    fix carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-layout row wrap>
+    <v-layout row wrap >
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
         <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
       </v-flex>
@@ -13,8 +13,15 @@
         <v-carousel-item
           v-for="meetup in meetups"
           :src="meetup.imageUrl"
-          :key="meetup.id"></v-carousel-item>
+          :key="meetup.id">
+            <div class="title"> {{ meetup.title }}</div>
+          </v-carousel-item>
       </v-carousel>
+    </v-layout>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups</p>  
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -31,3 +38,13 @@
     }
   }
 </script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0, 5);
+    color: white;
+    padding: 30px;
+  }
+</style>
