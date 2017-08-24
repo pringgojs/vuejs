9859bd7a9a81f90a391e3abb5b7b156d53a2a643
pringgo <pringgo@point.red>
2017-08-24 10:16:53 +0700

    fix responsive

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
-    
+
     <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile v-for="item in menuItems"
-        :key="item.title"
-        router
-        :to="item.link">
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -16,22 +16,25 @@
     </v-navigation-drawer>
 
     <v-toolbar dark class="primary">
-      <v-toolbar-side-icon 
+      <v-toolbar-side-icon
         @click.stop="sideNav = !sideNav"
-        class="hidden-sm-and-up">
-      </v-toolbar-side-icon>
+        class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items clas="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title">
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
-        </v-btn>  
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-
+    
     <main>
       <router-view></router-view>
     </main>
